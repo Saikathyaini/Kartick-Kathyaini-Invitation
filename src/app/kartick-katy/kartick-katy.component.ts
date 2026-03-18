c985d1257fd2884b8fdc1841096ce24590f43fd1
vidyajagana21 <vidyajagana21@gmail.com>
2026-03-18 10:01:23 +0530

    update

--- src/app/kartick-katy/kartick-katy.component.ts
+++ src/app/kartick-katy/kartick-katy.component.ts
@@ -1,29 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-kartick-katy',
   standalone: true,
-  imports:[CommonModule],
+  imports: [CommonModule],
   templateUrl: './kartick-katy.component.html',
   styleUrl: './kartick-katy.component.css'
 })
-export class KartickKatyComponent {
-opened = false;
-opening = false;
+export class KartickKatyComponent implements AfterViewInit {
 
-openInvitation() {
+  @ViewChild('bgMusic') music!: ElementRef;
 
-  this.opening = true;
-
-  const music = document.querySelector("audio") as HTMLAudioElement;
-  music.play();
-
-  setTimeout(()=>{
-    this.opened = true;
-  },1500);
-
-}
+  ngAfterViewInit(){
+    this.music.nativeElement.play().catch(()=>{});
+  }
 
 }
